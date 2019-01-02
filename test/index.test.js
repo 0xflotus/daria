881f7109c4bd59ee36ea74df9f807f4152ef4497
0xflotus <0xflotus@gmail.com>
2019-01-03 00:48:40 +0100

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -7,6 +7,12 @@ test("no args", () => {
   expect(stdout).toBe(mDate.getDate());
 });
 
+test("wrong date format", () => {
+  const { stdout } = execa("./index.js 2019-01-0");
+  const mDate = new ds.Darian_Date();
+  expect(stdout).toBe(mDate.getDate());
+})
+
 test("with specific date", () => {
   const { stdout } = execa("./index.js 2019-01-02");
   expect(stdout).toBe("Sol Saturni, 7 Tula 217, 10:08:00");
