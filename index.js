4732d6445804027891b59caf577df65c48ed8292
0xflotus <0xflotus@gmail.com>
2019-01-02 03:52:05 +0100

    added validate date check

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const ds = require("darian-system");
 
 const args = process.argv.slice(2);
 
-const marsDate = /\d{4}-\d{2}-\d{2}/.test(args[0])
-  ? new ds.Darian_Date(...args[0].split("-"))
-  : new ds.Darian_Date();
+try {
+  var marsDate = /\d{4}-\d{2}-\d{2}/.test(args[0])
+    ? new ds.Darian_Date(...args[0].split("-"))
+    : new ds.Darian_Date();
+} catch (err) {
+  console.warn("Error 101: Invalid date argument");
+  process.exit(-1);
+}
 
 if (args.length === 0) {
   console.log(marsDate.getDate());
