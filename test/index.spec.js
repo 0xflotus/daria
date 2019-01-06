c9e687fefd09ab0f42642947ca59d69f759486dd
0xflotus <0xflotus@gmail.com>
2019-01-06 01:41:06 +0100

    Update index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,24 +1,24 @@
 const execa = require("execa").shellSync;
 const ds = require("darian-system");
 
-test("no args", () => {
+test("test with no args", () => {
   const { stdout } = execa("node index.js");
   const mDate = new ds.Darian_Date();
   expect(stdout).toBe(mDate.getDate());
 });
 
-test.skip("wrong date format", () => {
+test.skip("test with wrong date format", () => {
   const { stdout } = execa("node index.js 2019-01-0");
   const mDate = new ds.Darian_Date();
   expect(stdout).toBe(mDate.getDate());
 })
 
-test("with specific date", () => {
+test("test with specific date", () => {
   const { stdout } = execa("node index.js 2019-01-02");
   expect(stdout).toBe("Sol Saturni, 7 Tula 217, 10:08:00");
 });
 
-test("with specific date and format", () => {
+test("test with specific date and format", () => {
   const { stdout } = execa('node index.js 2019-01-02 -f "Today is %D. %m %Y"');
   expect(stdout).toBe("Today is 7. Tula 217");
 });
